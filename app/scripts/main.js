ce53152d96c9a41f2688637dafb84b4689d35644
Levitskiy Denis <Proro4@gmail.com>
2018-08-01 18:08:55 +0300

    add animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+$(window).scroll(function () {
+    let scrollTop = $(this).scrollTop();
+    let windowHeight = $(window).height();
+    let scrollBottom = scrollTop + windowHeight;
+    let showDown = $('.showDown');
+    let showLeft = $('.showLeft');
+    let showRight = $('.showRight');
+    showDown.each(function () {
+        if(scrollBottom >= $(this).position().top){
+            $(this).addClass('active');
+        };
+    });
+    showLeft.each(function () {
+        if(scrollBottom >= $(this).position().top){
+            $(this).addClass('active');
+        };
+    });
+    showRight.each(function () {
+        if(scrollBottom >= $(this).position().top){
+            $(this).addClass('active');
+        };
+    });
+});
